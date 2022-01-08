c45954d517c6f31f5de3af252d6fda4c13997754
Reid <touyalin@gmail.com>
2022-01-08 20:12:32 +0800

    finish listing articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  default_scope { order(created_at: :desc) }
+
   validates :title, presence: true
   validates :body, presence: true
 end
---
+++ spec/features/listing_artilce_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'List artiles' do
+  before do
+    @article1 = Article.create(title: 'first article', body: 'Lorem, ipsum dolor sit amet...')
+    @article2 = Article.create(title: 'second article', body: 'Lorem, ipsum dolor sit amet consectetur...')
+  end
+
+  scenario 'a user lists all articles' do
+    visit '/'
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+  end
+
+  scenario 'a user has no article' do
+    Article.delete_all
+
+    visit '/'
+
+    expect(page).not_to have_content(@article1.title)
+    expect(page).not_to have_content(@article1.body)
+    expect(page).not_to have_content(@article2.title)
+    expect(page).not_to have_content(@article2.body)
+    expect(page).not_to have_link(@article1.title)
+    expect(page).not_to have_link(@article2.title)
+
+    within('h2#no-articles') do
+      expect(page).to have_content("No Articles Created.")
+    end
+  end
+end
