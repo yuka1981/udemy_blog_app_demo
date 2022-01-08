7aee4236334f6ff1aae5384bed9d156d7e1692d7
Reid <touyalin@gmail.com>
2022-01-08 18:20:15 +0800

    finish article validation

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -13,4 +13,38 @@ RSpec.feature 'Creating article' do
     expect(page).to have_content('Article has been created.')
     expect(page.current_path).to eq(articles_path)
   end
+
+  scenario 'a user create a new article without title' do
+    visit '/'
+    click_link 'New Article'
+    fill_in 'Title', with: ''
+    fill_in 'Body', with: 'Body content'
+    click_button 'Create Article'
+
+    expect(page).to have_content('Article has not been created.')
+    expect(page).to have_content("Title can't be blank")
+  end
+
+  scenario 'a user create a new article without content body' do
+    visit '/'
+    click_link 'New Article'
+    fill_in 'Title', with: 'New article'
+    fill_in 'Body', with: ''
+    click_button 'Create Article'
+
+    expect(page).to have_content('Article has not been created.')
+    expect(page).to have_content("Body can't be blank")
+  end
+
+  scenario 'a user create a new article with empty title and content body' do
+    visit '/'
+    click_link 'New Article'
+    fill_in 'Title', with: ''
+    fill_in 'Body', with: ''
+    click_button 'Create Article'
+
+    expect(page).to have_content('Article has not been created.')
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
+  end
 end
